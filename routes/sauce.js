95c52a16650eb63687273f4d3a6574458fd5b4ef
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-01-17 20:39:05 +0000

    error fixed

--- routes/sauce.js
+++ routes/sauce.js
@@ -8,9 +8,8 @@ const multer = require('../middleware/multer');
 router.get("/", auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.post('/:id/like', auth, sauceCtrl.likeSauce);
-// like a sauce
 
 module.exports = router;
